1e86c543803e14ee7e017413a0a6db4f30b0fc48
ElDiabloApps <carlos.sajim@gmail.comm>
2025-06-29 18:49:19 -0600

    Corrección final de config de Vite y app

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  // Añadimos esta sección para asegurar que el entorno de construcción
+  // sea compatible con las características modernas de JavaScript que usamos.
+  build: {
+    target: 'esnext'
+  }
 })
